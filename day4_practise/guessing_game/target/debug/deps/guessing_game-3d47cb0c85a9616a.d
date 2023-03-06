a3597d5957a87c18fa745d6bd660b7e7ccd502a1
Qucy Wei Qiu <tracyqucy@gmail.com>
2023-03-06 22:06:13 +0800

    hands on project

---
+++ day4_practise/guessing_game/target/debug/deps/guessing_game-3d47cb0c85a9616a.d
@@ -0,0 +1,5 @@
+/Users/qucy/rust_projects/LearnRust30MinsPerDay/day4_practise/guessing_game/target/debug/deps/guessing_game-3d47cb0c85a9616a: src/main.rs
+
+/Users/qucy/rust_projects/LearnRust30MinsPerDay/day4_practise/guessing_game/target/debug/deps/guessing_game-3d47cb0c85a9616a.d: src/main.rs
+
+src/main.rs:
