7c96ee9881e7c654345683088c20cb47cfed2a06
Qucy Wei Qiu <tracyqucy@gmail.com>
2023-03-06 22:08:41 +0800

    remove file

--- day4_practise/guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/qucy/rust_projects/LearnRust30MinsPerDay/day4_practise/guessing_game/target/debug/guessing_game: /Users/qucy/rust_projects/LearnRust30MinsPerDay/day4_practise/guessing_game/src/main.rs
