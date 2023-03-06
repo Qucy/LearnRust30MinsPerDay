6a1022de6e216422379343a9e1569e8c26cec2dd
Qucy Wei Qiu <tracyqucy@gmail.com>
2023-03-06 22:10:02 +0800

    remove target folder

--- day5_practise/guessing_game/target/debug/build/libc-ef05529cd43e0aeb/build_script_build-ef05529cd43e0aeb.d
+++
@@ -1,5 +0,0 @@
-/Users/qucy/rust_projects/LearnRust30MinsPerDay/day4_practise/guessing_game/target/debug/build/libc-ef05529cd43e0aeb/build_script_build-ef05529cd43e0aeb: /Users/qucy/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.139/build.rs
-
-/Users/qucy/rust_projects/LearnRust30MinsPerDay/day4_practise/guessing_game/target/debug/build/libc-ef05529cd43e0aeb/build_script_build-ef05529cd43e0aeb.d: /Users/qucy/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.139/build.rs
-
-/Users/qucy/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.139/build.rs:
