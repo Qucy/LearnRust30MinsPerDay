dd4452c431af9efc951fa1aa6e4d6c13b66afa07
Qucy Wei Qiu <tracyqucy@gmail.com>
2023-08-22 22:55:33 +0800

    update guessing game, add loop control and exception handle

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,24 +4,29 @@ use std::io;
 
 fn main() {
     println!("Please input your guess.");
-
-    let mut guess = String::new();
-
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is: {}", secret_number);
+    loop {
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Too big!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!!");
+                break;
+            }
+        }
+
+        println!("You guessed: {}", guess)
     }
-
-    print!("You guessed: {}", guess)
 }
