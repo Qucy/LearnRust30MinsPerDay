15ca090a6036bd101264f41e755edf35f3983513
Qucy Wei Qiu <tracyqucy@gmail.com>
2023-09-02 21:02:36 +0800

    learning struct method implementation and associated function

--- rec_area_calc/src/main.rs
+++ rec_area_calc/src/main.rs
@@ -1,3 +1,5 @@
+// Below code demostates how to use struct and method in Rust
+
 // This program calculates the area of a rectangle. (normal version)
 // fn main() {
 //     let w = 30;
@@ -28,20 +30,64 @@ struct Rectangle {
     height: u32,
 }
 
+// struct implementation
+impl Rectangle {
+    // method implementation
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    // method implementation
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    // associated function implementation
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
-    let rect = Rectangle {
+    let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect)
-    );
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    let square = Rectangle::square(3);
 
     // println!("rect is", rect); // will not compile due to missing trait `std::fmt::Display`
-    println!("rect is {:?}", rect); // debug print
-    println!("rect is {:#?}", rect); // pretty print
+    println!("rect is {:?}", rect1); // debug print
+    println!("rect is {:#?}", rect1); // pretty print
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    println!("rect1 can hold rect2? {}", rect1.can_hold(&rect2));
+
+    println!("rect1 can hold rect3? {}", rect1.can_hold(&rect3));
+
+    println!("square is {:?}", square);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
